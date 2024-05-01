0d278ba6c6908ccaedad9cdc612b7f70001c8a75
ajitkumar7432 <156606144+ajitkumar7432@users.noreply.github.com>
2024-05-01 11:33:44 +0530

    Add files via upload

---
+++ SpringProgram5/src/in/sp/beans/Address.java
@@ -0,0 +1,22 @@
+package in.sp.beans;
+
+public class Address {
+	private int houseno;
+	private String city;
+	private int pincode;
+	public void setHouseno(int houseno) {
+		this.houseno = houseno;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+
+@Override
+public string toString() {
+	return "#"+houseno+","+city+"-"+pincode;
+}
+
+}
